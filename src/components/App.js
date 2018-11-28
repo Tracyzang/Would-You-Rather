6b762ac982ed0a871681180acd18d6ab057495aa
MacUser01 <macuser01@MacUser01matoMacBook-Air.local>
2018-11-28 14:07:51 -0600

    update

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        WOULD YOU RATHER
+      </div>
+    );
+  }
+}
+
+export default App;
